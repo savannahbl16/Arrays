e1e24692b68cf0e81a1330b102d1d2e57a34ed4c
Savannah Lo <slo42@gatech.edu>
2020-07-23 13:03:15 -0400

    added functionality comments

--- Strings/src/FirstUniqueInString.java
+++ Strings/src/FirstUniqueInString.java
@@ -2,27 +2,30 @@ import java.util.Arrays;
 
 public class FirstUniqueInString {
     public int firstUniqChar(String s) {
+        //initial edge case checks
         if (s.length() == 0) {
             return -1;
         }
         if (s.length() == 1) {
             return 0;
         }
+        // turn string into sorted array of characters so duplicates are next to each other
         char [] arr = s.toCharArray();
         Arrays.sort(arr);
-        int indx = Integer.MAX_VALUE;
+        int indx = Integer.MAX_VALUE; // initialize to max value so that the index of the first unique char will be updated if there is one
         for (int i = 0; i < arr.length; i++) {
+            //if last element or the element is not a duplicate then set indx to the index of the element in orginial string
             if ((i+1 == arr.length) || arr[i] != arr[i+1]) {
                 if (s.indexOf(arr[i]) < indx) {
                     indx = s.indexOf(arr[i]);
                 }
-            } else {
+            } else { // otherwise, move past the duplicates
                 while (i != arr.length - 1 && (arr[i] == arr[i+1])) {
                     i++;
                 }
             }
         }
-        if (indx == Integer.MAX_VALUE) {
+        if (indx == Integer.MAX_VALUE) { // no non-duplicate found
             indx = -1;
         }
         return indx;
--- Strings/src/ReverseString.java
+++ Strings/src/ReverseString.java
@@ -1,5 +1,7 @@
 public class ReverseString {
     public void reverseString(char[] s) {
+        // take all the elements in the first half and swap them with the symmetrical element on second half
+        // if the number of elements is odd, the middle element remains the same
         for (int i = 0; i < s.length / 2; i++) {
             char temp = s[i];
             s[i] = s[s.length - i - 1];
